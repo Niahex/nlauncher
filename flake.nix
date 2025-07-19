4471dbc6c0b636f5c2d853a190d2e295bf138ace
Niahex <tqm6fa6b@duck.com>
2025-07-19 07:10:50 +0200

    flake update

--- flake.nix
+++ flake.nix
@@ -5,11 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     crane.url = "github:ipetkov/crane";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = {
@@ -20,99 +16,93 @@
     rust-overlay,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [ (import rust-overlay) ];
-      pkgs = import nixpkgs { inherit system overlays; };
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        # Overlays and package set
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {inherit system overlays;};
 
-      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-        extensions = [ "rust-src" ];
-      };
+        # Rust toolchain configuration
+        rustToolchain = pkgs.rust-bin.stable."1.88.0".default.override {
+          extensions = ["rust-src"];
+        };
 
-      craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-      src = craneLib.cleanCargoSource ./.;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        src = craneLib.cleanCargoSource ./.;
 
-      buildInputs = with pkgs; [
-        gtk4
-        gtk4-layer-shell
-        glib
-        pango
-        gdk-pixbuf
-        wayland
-        wayland-protocols
-        dbus
-      ];
+        # Common dependencies
+        buildInputs = with pkgs; [
+          gtk4
+          gtk4-layer-shell
+          glib
+          pango
+          gdk-pixbuf
+          wayland
+          wayland-protocols
+          dbus
+        ];
 
-      nativeBuildInputs = with pkgs; [
-        pkg-config
-        makeWrapper
-        wrapGAppsHook4
-      ];
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          makeWrapper
+          wrapGAppsHook4
+        ];
 
-      envVars = {
-        RUST_BACKTRACE = "full";
-      };
+        envVars = {
+          RUST_BACKTRACE = "full";
+        };
 
-      cargoArtifacts = craneLib.buildDepsOnly {
-        inherit src buildInputs nativeBuildInputs;
-        env = envVars;
-      };
+        # Build artifacts
+        cargoArtifacts = craneLib.buildDepsOnly {
+          inherit src buildInputs nativeBuildInputs;
+          env = envVars;
+        };
 
-      nlauncher = craneLib.buildPackage {
-        inherit src cargoArtifacts buildInputs nativeBuildInputs;
-        env = envVars;
-        pname = "nlauncher";
-        version = "0.1.0";
-        postInstall = ''
-          install -d $out/share/glib-2.0/schemas
-          cat > $out/share/glib-2.0/schemas/github.niahex.nlauncher.gschema.xml << EOF
-          <?xml version="1.0" encoding="UTF-8"?>
-          <schemalist>
-            <schema id="github.niahex.nlauncher" path="/github/niahex/nlauncher/">
-              <!-- Aucune clé de schéma pour le moment -->
-            </schema>
-          </schemalist>
-          EOF
-        '';
-      };
-    in {
-      packages = {
-        default = nlauncher;
-        nlauncher = nlauncher;
-      };
-
-      checks = {
-        inherit nlauncher;
-
-        nlauncher-clippy = craneLib.cargoClippy {
+        # Application package definition
+        nlauncher = craneLib.buildPackage {
           inherit src cargoArtifacts buildInputs nativeBuildInputs;
           env = envVars;
-          cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          pname = "nlauncher";
+          version = "0.1.0";
         };
 
-        nlauncher-fmt = craneLib.cargoFmt {
-          inherit src;
-        };
-      };
-
-      devShells.default = pkgs.mkShell {
-        inputsFrom = [nlauncher];
-        nativeBuildInputs = with pkgs; [
+        # Development shell tools
+        devTools = with pkgs; [
           rust-analyzer
           rustToolchain
           cargo-watch
           cargo-edit
           bacon
         ];
+      in {
+        packages = {
+          default = nlauncher;
+          inherit nlauncher;
+        };
 
-        env = envVars;
+        checks = {
+          inherit nlauncher;
 
-        shellHook = ''
-          echo "[🦀 Rust $(rustc --version)] - Ready !"
-          echo "Dépendances: ${pkgs.lib.concatStringsSep " " (map (p: p.name) nativeBuildInputs)}"
-          echo "Available commands: cargo watch, cargo edit, bacon"
-        '';
-      };
+          nlauncher-clippy = craneLib.cargoClippy {
+            inherit src cargoArtifacts buildInputs nativeBuildInputs;
+            env = envVars;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          };
 
-      formatter = pkgs.alejandra;
-    });
+          nlauncher-fmt = craneLib.cargoFmt {inherit src;};
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [nlauncher];
+          nativeBuildInputs = devTools;
+          env = envVars;
+
+          shellHook = ''
+            echo "[🦀 Rust $(rustc --version)] - Ready to develop nlauncher!"
+          '';
+        };
+
+        formatter = pkgs.alejandra;
+      }
+    );
 }
